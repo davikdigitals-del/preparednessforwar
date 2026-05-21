fc3a530bff5f99493f4880f91b691922d74b839e
Ajibola Gbenga Joseph <ajibolagbengajoseph@gmail.com>
2026-05-21 23:29:09 +0100

    Force redeploy Twilio edge functions to pick up secrets

--- supabase/functions/twilio-voice-token/index.ts
+++ supabase/functions/twilio-voice-token/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
+// v2 - force redeploy to pick up Supabase secrets
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
