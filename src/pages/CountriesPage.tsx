37ce7efb82b1449484515a0528951daec9540c4c
davikdigitals-del <davikdigitals@gmail.com>
2026-08-02 01:44:39 +0100

    Countries: flag cards, remove panels, world map display-only

--- src/pages/CountriesPage.tsx
+++ src/pages/CountriesPage.tsx
@@ -280,7 +280,7 @@ const CountriesPage = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8V4m0 0h4M4 4l5 5m11-5h-4m4 0v4m0-4l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
                 </svg>
               </button>
-              <div className="absolute inset-0">
+              <div className="absolute inset-0 pointer-events-none">
                 <InteractiveWorldMap height="100%" />
               </div>
             </div>
