88ab9e7fa4154e88e672544c1aef54ac911379e5
davikdigitals-del <davikdigitals@gmail.com>
2026-08-05 10:28:44 +0100

    fix: restore single header bar, clock time, categories fix

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "c:\Users\EMMAX\Documents\pre\preparednessforwar"
+git add -A
+git commit -m "fix: single header bar, clock shows time, AdminPosts no categories DB calls"
+git push origin main
+echo DONE
+pause
